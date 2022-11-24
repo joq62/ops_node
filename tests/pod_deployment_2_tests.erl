f3d500c05ab29239ab55d4cfaa4c54be6bf86ba2
joq62 <joakim.leche@gmail.com>
2022-11-25 00:37:34 +0100

    ud

--- tests/pod_deployment_2_tests.erl
+++ tests/pod_deployment_2_tests.erl
@@ -56,9 +56,7 @@ read_specs_test()->
 
     %%%
     PodDeployInfoResult=pod_deployment_info(ClusterApplicationDeployments),
-  %  PodDeployInfoList=pod_deployment_info(ApplDeploymentSpecs,WorkerHostSpecs),
-   % [true,true]=[lists:member({spec_id,"math"},L)||L<-PodDeployInfoList],
-  
+    
 %% Store in db_etcd
 %% DeployId, Stats, PodDeployInfo + Status candidate, deployed 
    % [
@@ -86,39 +84,43 @@ pod_deployment_info([],Acc)->
     Acc;
 pod_deployment_info([ClusterApplicationDeploymentId|T],Acc) ->
 
-    {ok,ClusterDeploymentId}=ops_node:cluster_application_deployments(cluster_specs,ClusterApplicationDeploymentId),
+    {ok,ClusterDeploymentId}=db_cluster_application_deployment:read(cluster_spec,ClusterApplicationDeploymentId),
+    {ok,ApplDeploymentSpecs}=ops_node:cluster_application_deployments(appl_deployment_specs,ClusterApplicationDeploymentId), 
     {ok,WorkerHostSpecs}=db_cluster_deployment:read(worker_host_specs,ClusterDeploymentId),
-    {ok,ApplDeploymentSpecs}=ops_node:cluster_application_deployments(appl_deployment_specs,ClusterApplicationDeploymentId),
-  %  DeplId=erlang:integer_to_list(os:system_time(microsecond),36)++"_pod_info",
-    glurk=pod_deployment_info(ApplDeploymentSpecs,WorkerHostSpecs,[]),
     
+    PodApplInfoList=pod_deployment_info(ApplDeploymentSpecs,WorkerHostSpecs,[]),
+    [ok,ok]=db_create_pod_info(PodApplInfoList,ClusterApplicationDeploymentId),
+    
+    ["GFQ8ZLXHXM_pod_info","GFQ8ZLXHF0_pod_info"]=db_pod_info:get_all_id(),
 
 
-  %  R=ops_node:create_pod_info(DeplId,ApplDeployId,Status,
-%			       PodName,PodNode,PodDir,
-%			       ClusterApplicationDeploymentId,ClusterSpecId,
-%			       HostSpecId,
-%			       ApplSpecId),
-    
+%    [
+%     {"GFQ81UQG8I_pod_info",glurk,candidate,"GFQ81UQFAC_pod",'GFQ81UQFAC_pod@c100',
+%      "GFQ81UQFAC_pod.dir","a","many_c100_c200","c100","math"
+%     },
+%     {"GFQ81UQFGU_pod_info",glurk,candidate,"GFQ81UQFGP_pod",'GFQ81UQFGP_pod@c200',
+%      "GFQ81UQFGP_pod.dir","a","many_c100_c200","c200","math"}]=db_pod_info:read_all(),
+       
     pod_deployment_info(T,[glurk|Acc]).
 
 pod_deployment_info([],WorkerHostSpecs,Acc)->
     Acc;
 pod_deployment_info([ApplDeploymentSpec|T],WorkerHostSpecs,Acc) ->
- %   {_SpecId,ApplSpec,Vsn,NumInstances,[]}=ops_node:application_deployment_info(ApplDeploymentSpec),
     PodInfoList=appl_info(ApplDeploymentSpec,WorkerHostSpecs),
-  %  NewAcc=lists:append(PodInfoList,Acc),
-    NewAcc=[PodInfoList|Acc],
+    NewAcc=lists:append(PodInfoList,Acc),
     pod_deployment_info(T,WorkerHostSpecs,NewAcc).
    
 
 %% Affinity = []
 
-appl_info({SpecId,ApplSpec,Vsn,NumInstances,[]},HostSpecList)->
-    appl_info(SpecId,ApplSpec,Vsn,NumInstances,HostSpecList,[]).
-appl_info(_SpecId,_ApplSpec,_Vsn,0,_HostSpecList,Acc)->    
+appl_info(ApplDeploymentSpec,HostSpecList)->
+    {SpecId,ApplSpec,Vsn,NumInstances,Affinity}=ops_node:application_deployment_info(ApplDeploymentSpec),
+    appl_info({SpecId,ApplSpec,Vsn,NumInstances,Affinity},HostSpecList,[]).
+
+
+appl_info({_SpecId,_ApplSpec,_Vsn,0,[]},_HostSpecList,Acc)->    
     Acc;
-appl_info(SpecId,ApplSpec,Vsn,N,[HostSpec|T],Acc)->
+appl_info({SpecId,ApplSpec,Vsn,NumInstances,[]},[HostSpec|T],Acc)->
     {ok,HostName}=db_host_spec:read(hostname,HostSpec),
     PodName=erlang:integer_to_list(os:system_time(microsecond),36)++"_pod",  
     PodNode=list_to_atom(PodName++"@"++HostName),
@@ -128,13 +130,58 @@ appl_info(SpecId,ApplSpec,Vsn,N,[HostSpec|T],Acc)->
 		{pod_dir,PodDir}],
     NewAcc=[Info|Acc],
     RotatedHostSpecList=lists:append(T,[HostSpec]),
-    appl_info(SpecId,ApplSpec,Vsn,N-1,RotatedHostSpecList,NewAcc).
+    appl_info({SpecId,ApplSpec,Vsn,NumInstances-1,[]},RotatedHostSpecList,NewAcc).
  
 %% --------------------------------------------------------------------
 %% Function: available_hosts()
 %% Description: Based on hosts.config file checks which hosts are avaible
 %% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
 %% --------------------------------------------------------------------
+db_create_pod_info(PodApplInfoList,ClusterApplicationDeploymentId)->
+   
+    db_create_pod_info(PodApplInfoList,ClusterApplicationDeploymentId,[]).
+
+db_create_pod_info([],_ClusterApplicationDeploymentId,Acc)->
+    Acc;
+db_create_pod_info([PodApplInfo|T],ClusterApplicationDeploymentId,Acc)->
+    {ok,ClusterDeploymentId}=db_cluster_application_deployment:read(cluster_spec,ClusterApplicationDeploymentId),
+    {ok,ApplDeploymentSpecs}=ops_node:cluster_application_deployments(appl_deployment_specs,ClusterApplicationDeploymentId), 
+  
+    NewAcc=case db_store_pod_info(PodApplInfo,ClusterApplicationDeploymentId,[]) of
+	       {error,Reason}->
+		   [{error,[Reason,PodApplInfo]}|Acc];
+	       ok->
+		   [ok|Acc]
+	   end,
+    db_create_pod_info(T,ClusterApplicationDeploymentId,NewAcc).
+
+db_store_pod_info(PodApplInfo,ClusterApplicationDeploymentId,Acc)->
+
+    DeplId=erlang:integer_to_list(os:system_time(microsecond),36)++"_pod_info",  
+    Status=candidate,
+    ApplDeployId=glurk,
+    {ok,ClusterDeploymentId}=db_cluster_application_deployment:read(cluster_spec,ClusterApplicationDeploymentId),
+    {host_spec,HostSpecId}=lists:keyfind(host_spec,1,PodApplInfo),
+    {appl_spec,ApplSpecId}=lists:keyfind(appl_spec,1,PodApplInfo),
+    {pod_name,PodName}=lists:keyfind(pod_name,1,PodApplInfo),
+    {pod_node,PodNode}=lists:keyfind(pod_node,1,PodApplInfo),
+    {pod_dir,PodDir}=lists:keyfind(pod_dir,1,PodApplInfo),
+   
+    Result=case db_pod_info:create(DeplId,ApplDeployId,Status,
+		       PodName,PodNode,PodDir,
+		       ClusterApplicationDeploymentId,
+		       ClusterDeploymentId,
+		       HostSpecId,
+		       ApplSpecId) of
+	       {error,Reason}->
+		   {error,Reason};
+	       {atomic,ok}->
+		   ok
+	   end,
+    Result.
+   
+    
+
 
 %% --------------------------------------------------------------------
 %% Function: available_hosts()
